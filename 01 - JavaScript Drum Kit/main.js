a69dc309f6f72710f0b037f111079bd2befd76ab
arrahhal <arrahhal@proton.me>
2025-07-24 01:05:58 +0300

    refactor solution after watching the video

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -10,14 +10,14 @@ document.addEventListener("keydown", (e) => {
     const div = document.querySelector(`div[data-key='${code}']`);
     const audio = document.querySelector(`audio[data-key='${code}']`);
     div.classList.add("playing");
+    audio.currentTime = 0;
     audio.play();
   }
 });
 
-document.addEventListener("keyup", (e) => {
-  const code = e.keyCode;
-  if (keyCodes.includes(code)) {
-    const div = document.querySelector(`div[data-key='${code}']`);
+document.addEventListener("transitionend", (e) => {
+  if (e.propertyName === "transform") {
+    const div = e.target;
     div.classList.remove("playing");
   }
 });
